080e5de70f2a133cfbf97f902bceaf16c049cf5f
Bheemesh <gbheemesh888.com>
2023-02-25 16:40:12 +0530

    Done7

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -21,8 +21,8 @@ public class OrderController {
 
 
 
-    @Autowired
-    OrderService orderService;
+   // @Autowired
+    OrderService orderService = new OrderService();
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
 
